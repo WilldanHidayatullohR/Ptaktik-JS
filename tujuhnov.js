9285f74868efc6599a995547acecec151e987fd4
Wildan Hidayatulloh <wildanhidayatulloh73@gmail.com>
2024-11-07 11:55:21 +0700

    Update tujuhnov.js

--- tujuhnov.js
+++ tujuhnov.js
@@ -1,24 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
   let antrian = ["ray", "fiki", "fadhilla", "farah"];
 
-  // Modifikasi antrian sesuai instruksi
   antrian.push("nabila");
   antrian.push("maza", "elsi");
   antrian.pop();
   antrian.shift();
   antrian.shift();
   antrian.unshift("tomi");
-
-  // Ambil elemen <ul> dengan id "antrian-list"
+  
   const antrianList = document.getElementById("antrian-list");
 
-  // Buat elemen <li> untuk setiap pembeli di antrian
   antrian.forEach((pembeli) => {
     const listItem = document.createElement("li");
     listItem.textContent = pembeli;
     antrianList.appendChild(listItem);
 
-    // Menampilkan tiap pembeli di console, satu per baris
     console.log(pembeli);
   });
 });
