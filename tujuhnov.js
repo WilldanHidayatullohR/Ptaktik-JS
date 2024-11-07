bacabf5fbe129b9eee5d05183725157d9149d703
Wildan Hidayatulloh <wildanhidayatulloh73@gmail.com>
2024-11-07 11:26:27 +0700

    Update tujuhnov.js

--- tujuhnov.js
+++ tujuhnov.js
@@ -1,23 +1,30 @@
-// Inisialisasi antrian awal
+//  antrian awal
 let antrian = ["Ray", "Fiki", "Fadhilla", "Farah"];
+console.log("Antrian awal:", antrian);
 
-// Nabila datang
+// 1. Kak Nabila datang ke antrian, kita tambahkan dia di belakang
 antrian.push("Nabila");
+console.log("Setelah Nabila datang:", antrian);
 
-// Maza dan Elsi datang
+// 2. Kak Maza dan Kak Elsi datang ke antrian, tambahkan keduanya di belakang
 antrian.push("Maza", "Elsi");
+console.log("Setelah Maza dan Elsi datang:", antrian);
 
-// Elsi pulang
+// 3. Kak Elsi, yang berada di paling belakang, pulang ke rumah
 antrian.pop();
+console.log("Setelah Elsi pulang:", antrian);
 
-// Ray mendapatkan belanjaannya
+// 4. Ray yang berada di depan sudah mendapatkan belanjaannya
 antrian.shift();
+console.log("Setelah Ray mendapatkan belanjaannya:", antrian);
 
-// Fiki mendapatkan belanjaannya
+// 5. Fiki, yang sekarang berada di depan, juga sudah mendapatkan belanjaannya
 antrian.shift();
+console.log("Setelah Fiki mendapatkan belanjaannya:", antrian);
 
-// Tomi nyerobot antrian di depan
+// 6. Kak Tomi nyerobot antrian di posisi terdepan
 antrian.unshift("Tomi");
+console.log("Setelah Tomi nyerobot:", antrian);
 
 // Hasil akhir antrian
 console.log("Hasil akhir antrian:", antrian);
